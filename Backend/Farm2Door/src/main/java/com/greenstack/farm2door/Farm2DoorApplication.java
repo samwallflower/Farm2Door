2f720b0d65a09152a529c0f6e040b74f8c753540
Samanta <samwallflower197127@gmail.com>
2025-10-03 00:04:04 +0200

    Initial Project Setup Backend added.

---
+++ Backend/Farm2Door/src/main/java/com/greenstack/farm2door/Farm2DoorApplication.java
@@ -0,0 +1,13 @@
+package com.greenstack.farm2door;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Farm2DoorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Farm2DoorApplication.class, args);
+    }
+
+}

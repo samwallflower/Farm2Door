93f705c8fb6a6193f6cae663ab3f041d3d3bd87a
kimoh <kimohussain6@gmail.com>
2025-11-21 20:25:30 +0100

    CartItemController created

---
+++ Backend/Farm2Door/src/main/java/com/greenstack/farm2door/controller/CartItemController.java
@@ -0,0 +1,67 @@
+package com.greenstack.farm2door.controller;
+
+
+import com.greenstack.farm2door.exceptions.ResourceNotFoundException;
+import com.greenstack.farm2door.model.Cart;
+import com.greenstack.farm2door.model.User;
+import com.greenstack.farm2door.response.ApiResponse;
+import com.greenstack.farm2door.service.cart.ICartItemService;
+import com.greenstack.farm2door.service.cart.ICartService;
+import com.greenstack.farm2door.service.user.IUserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.http.HttpStatus.UNAUTHORIZED;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("${api.prefix}/cartItems")
+public class CartItemController {
+    private final ICartItemService cartItemService;
+    private final ICartService cartService;
+    private final IUserService userService;
+
+    @PostMapping("/item/add")
+    public ResponseEntity<ApiResponse> addItemToCart(@RequestParam Long productId,
+                                                     @RequestParam int quantity) {
+        try {
+            User user = userService.getAuthenticatedUser();
+
+            Cart cart = cartService.initializeNewCart(user);
+
+            cartItemService.addItemToCart(cart.getId(), productId, quantity);
+            return ResponseEntity.ok(new ApiResponse("Item added to cart successfully", null));
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.status(NOT_FOUND)
+                    .body(new ApiResponse( e.getMessage(), null));
+        }catch (JwtException e){
+            return ResponseEntity.status(UNAUTHORIZED)
+                    .body(new ApiResponse(e.getMessage(), null));
+        }
+    }
+    @DeleteMapping("/cart/{cartId}/item/{productId}/remove")
+    public ResponseEntity<ApiResponse> removeItemFromCart(  @PathVariable Long cartId,
+                                                            @PathVariable Long productId) {
+        try {
+            cartItemService.removeItemFromCart(cartId, productId);
+            return ResponseEntity.ok(new ApiResponse("Item removed from cart successfully", null));
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.status(NOT_FOUND)
+                    .body(new ApiResponse(e.getMessage(), null));
+        }
+    }
+    @PutMapping("/cart/{cartId}/item/{productId}/update")
+    public ResponseEntity<ApiResponse> updateItemQuantity( @PathVariable Long cartId,
+                                                           @PathVariable Long productId,
+                                                           @RequestParam int quantity) {
+        try {
+            cartItemService.updateItemQuantity(cartId, productId, quantity);
+            return ResponseEntity.ok(new ApiResponse("Item quantity updated successfully", null));
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.status(NOT_FOUND)
+                    .body(new ApiResponse(e.getMessage(), null));
+        }
+    }
+}
